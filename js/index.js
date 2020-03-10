019d98ffe9d17e9594d1db3d3d10457ce9151dfb
Zoran Stanic <printerstampac@gmail.com>
2020-03-10 02:46:11 +0100

    updated share logic

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-  $("button").on("click", function(){
+  $("#new").on("click", function(){
 $.getJSON("https://jsonp.afeld.me/?url=http://quotes.stormconsultancy.co.uk/random.json?callback=",function(a) {
   $(".quote").html("<h3>" + a.quote + "</h3>" + "<h5>" + a.author + "</h5>");
 });
   });
-  
+  $("#tweet").click(function() {
+  var phrase = document.getElementById("new").innerText;
+  var tweetUrl = "https://twitter.com/share?text=" +
+    encodeURIComponent(phrase)";
+    
+  window.open(tweetUrl);
+});
 
   });
